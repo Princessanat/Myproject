4514b6ef522f686c66d82e3577b9e22d0ac5b20e
Natalia Sivtseva <natalia-2201@yandex.ru>
2024-06-17 20:29:23 +0300

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,28 @@
+package Myproject;
+import java.util.Scanner;
+import static java.util.Arrays.deepToString;
+public class Phone {
+    //КАК ПОЛУЧИЛОСЬ, ТАК ПОЛУЧИЛОСЬ, КОД НЕ РАБОТАЕТ))
+}
+    public static void main(String[] args) {
+        Integer[] array = new Integer[10];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(getНовыйКонтакт(new);
+        private static String getНовыйКонтакт();
+        for(int i = 0; i <=10; i++){
+            array[i] = sc.nextInt();
+        }
+
+        System.out.println("Новый контакт");
+        System.out.println("Редактировать контакт");
+
+        System.out.println("Удалить контакт");
+        System.out.println("Выйти");
+        System.out.println(deepToString(array));
+    private static String getНовыйКонтакт() {
+    return "Новый контакт";
+}
+{
+
+}
---
+++ Сontact.java
@@ -0,0 +1,23 @@
+package Myproject;
+public class Contact {
+
+    //Поля
+    private String name;
+    String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName) {
+        if (newName != "") {
+            name = newName;
+            private String value;
+            String value;
+            }
+        (String newValue);
+    }
+    public newName(String name)
+    {
+        this.name = name;
+    }
+
+}
